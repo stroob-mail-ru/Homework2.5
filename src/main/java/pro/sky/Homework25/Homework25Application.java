cf1e1e634fa29253e0a5173f6b8d6d983224dcc0
Андрей <stroob@mail.ru>
2024-03-05 00:14:43 +0700

    Initial commit

---
+++ src/main/java/pro/sky/Homework25/Homework25Application.java
@@ -0,0 +1,13 @@
+package pro.sky.Homework25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework25Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Homework25Application.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/Homework25/Homework25ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.Homework25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework25ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
